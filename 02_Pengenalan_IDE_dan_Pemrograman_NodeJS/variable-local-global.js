1e9b7b916d46f29a19d1b4e6120f4d9183ff03ec
MartryatusSofia <martryatuss@gmail.com>
2025-02-24 12:13:25 +0700

    Pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,11 @@
+let globalVariable = "Martryatus Sofia"
+
+function getMyIdentity(){
+    let localVariable = 2311104003
+    console.log(`Nama saya ${globalVariable} dan nim saya ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variables.js
@@ -0,0 +1,23 @@
+//var
+
+var name = "Martryatus Sofia"
+const nim = 2311104003
+
+console.log(name)
+console.log("nama saya adalah" + name + " dan nim saya adalah " + nim)
+console.log(`nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+var name = "ayam"
+
+let fullName = "Martyatus Sofia"
+
+//const kelas
+//nim = 2311104003
+
+/*
+sofia 
+yows
+*/
