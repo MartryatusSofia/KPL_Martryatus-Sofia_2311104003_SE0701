ab6406c2c2f19bb687b7c5d82716297ddb1d792b
MartryatusSofia <martryatuss@gmail.com>
2025-03-23 11:44:57 +0700

    menambahkan class SimpleDataBase

--- 05_Generics/jmmodul5_2311104003/penjumlahan.js
+++ 05_Generics/jmmodul5_2311104003/penjumlahan.js
@@ -10,3 +10,32 @@ let angka2 = 11;
 let angka3 = 10; 
 
 Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+
+class SimpleDataBase {
+    constructor() {
+        this.storedData = [];
+        this.inputDates = [];
+    }
+
+    AddNewData(data) {
+        this.storedData.push(data);
+        this.inputDates.push(new Date().toISOString());
+    }
+
+    PrintAllData() {
+        this.storedData.forEach((data, index) => {
+            console.log(`Data ${index + 1} berisi: ${data}, yang disimpan pada waktu UTC: ${this.inputDates[index]}`);
+        });
+    }
+}
+
+
+const db = new SimpleDataBase();
+
+
+db.AddNewData(23);
+db.AddNewData(11);
+db.AddNewData(10);
+
+
+db.PrintAllData();
