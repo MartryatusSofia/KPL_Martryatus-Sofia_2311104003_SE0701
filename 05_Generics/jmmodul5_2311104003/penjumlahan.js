78e9627e1c0904b4d545813da151e8e8accaf4b9
MartryatusSofia <martryatuss@gmail.com>
2025-03-23 11:42:54 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_Generics/jmmodul5_2311104003/penjumlahan.js
@@ -0,0 +1,12 @@
+class Penjumlahan {
+    static JumlahTigaAngka(a, b, c) {
+        let hasil = a + b + c;
+        console.log(`Hasil penjumlahan: ${hasil}`);
+    }
+}
+
+let angka1 = 23; 
+let angka2 = 11; 
+let angka3 = 10; 
+
+Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
