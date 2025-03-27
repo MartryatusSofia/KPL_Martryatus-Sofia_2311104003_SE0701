ec992d16343f6ff4c1103c3d5a434f1af1c5f455
MartryatusSofia <martryatuss@gmail.com>
2025-03-27 23:11:58 +0700

    Implementasi design by contract

--- 06_Design by Contract/SayaTubeVideo.js
+++ 06_Design by Contract/SayaTubeVideo.js
@@ -1,12 +1,28 @@
 class SayaTubeVideo {
     constructor(title) {
+        if (!title || title.length > 100) {
+            throw new Error("Judul video tidak boleh null dan harus kurang dari 100 karakter.");
+        }
+        
         this.id = Math.floor(10000 + Math.random() * 90000); 
         this.title = title;
         this.playCount = 0;
     }
 
     increasePlayCount(count) {
-        this.playCount += count;
+
+        if (count > 10000000) {
+            throw new Error("Maksimal penambahan play count adalah 10.000.000.");
+        }
+        
+        try {
+            if (this.playCount + count > Number.MAX_SAFE_INTEGER) {
+                throw new Error("Terjadi overflow pada play count.");
+            }
+            this.playCount += count;
+        } catch (error) {
+            console.error("Error: ", error.message);
+        }
     }
 
     printVideoDetails() {
@@ -16,7 +32,14 @@ class SayaTubeVideo {
     }
 }
 
-let video = new SayaTubeVideo("Tutorial Design By Contract - [MARTRYATUS SOFIA]");
-video.printVideoDetails();
-video.increasePlayCount(500);
-video.printVideoDetails();
+try {
+    let video = new SayaTubeVideo("Tutorial Design By Contract - [MARTRYATUS SOFIA]");
+    video.printVideoDetails();
+
+    video.increasePlayCount(5000);
+    video.printVideoDetails();
+
+    video.increasePlayCount(10000001);
+} catch (error) {
+    console.error("Error: ", error.message);
+}
