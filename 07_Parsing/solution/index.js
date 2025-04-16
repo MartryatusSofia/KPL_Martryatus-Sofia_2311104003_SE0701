a2b47098c22a646ceda15083e6da0c427e57bdb5
MartryatusSofia <martryatuss@gmail.com>
2025-04-16 22:22:12 +0700

    menambahkan folder solution, dan file index, martryatus_2311104003, dan tp7_1_2311104003

---
+++ 07_Parsing/solution/index.js
@@ -0,0 +1,3 @@
+import { martryatus_2311104003 } from './martryatus_2311104003.js';
+
+martryatus_2311104003.ReadJSON();
---
+++ 07_Parsing/solution/martryatus_2311104003.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+
+export class martryatus_2311104003 {
+  static ReadJSON() {
+    const data = fs.readFileSync('./tp7_1_2311104003.json', 'utf-8');
+    const obj = JSON.parse(data);
+    console.log(`Nama ${obj.nama.depan} ${obj.nama.belakang} dengan nim ${obj.nim} dari fakultas ${obj.fakultas}`);
+  }
+}
