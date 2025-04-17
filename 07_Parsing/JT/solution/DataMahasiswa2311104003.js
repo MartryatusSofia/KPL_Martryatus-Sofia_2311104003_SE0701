518b797dcbbbb903a20aebb466303990de63211f
MartryatusSofia <martryatuss@gmail.com>
2025-04-17 07:10:10 +0700

    Add JSON parsing functionality for student and course data
    - Implemented DataMahasiswa2311104003 class to read and display student information from tp7_1_2311104003.json.
    - Created KuliahMahasiswa2311104003 class to read and list courses from tp7_2_2311104003.json.
    - Added corresponding index.js files to execute the reading functions for both student and course data.
    - Included sample JSON files for student and course data.

--- 07_Parsing/JT/solution/DataMahasiswa2311104003.js
+++ 07_Parsing/JT/solution/DataMahasiswa2311104003.js
@@ -4,6 +4,6 @@ export class DataMahasiswa2311104003 {
   static ReadJSON() {
     const data = fs.readFileSync('./tp7_1_2311104003.json', 'utf-8');
     const obj = JSON.parse(data);
-    console.log(`Nama ${obj.nama.depan} ${obj.nama.belakang} dengan nim ${obj.nim} dari fakultas ${obj.fakultas}`);
+    console.log(`Nama ${obj.nama.depan} ${obj.nama.belakang} dengan nim ${obj.nim} dari prodi ${obj.fakultas}`);
   }
 }
