e9ed8953be7ed6d45f1d84a2ecd76ace10cb1270
MartryatusSofia <martryatuss@gmail.com>
2025-04-26 21:58:03 +0700

    Menambahkan API Mahasiswa TPMODUL9

---
+++ 09_API Design dan Construction Using Swagger/tpmodul9_2311104003/mahasiswa.js
@@ -0,0 +1,7 @@
+const mahasiswa = [
+    { nama: "Martryatus Sofia", nim: "2311104003" },
+    { nama: "Sinta Alimah", nim: "23111040100" },
+    { nama: "Banyu", nim: "23111040101" },
+];
+
+module.exports = mahasiswa;
